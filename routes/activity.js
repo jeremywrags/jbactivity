8145a45331824ecb6bc0a36cfe6285080e1ee46e
jeremywrags@gmail.com <jragsdale@jragsdale-ltm4.internal.salesforce.com>
2019-09-25 13:13:10 -0500

    Added steps to activity

--- routes/activity.js
+++ routes/activity.js
@@ -50,7 +50,6 @@ function logData(req) {
 
 
 // Deps
-const Path = require('path');
 const JWT = require(Path.join(__dirname, '..', 'lib', 'jwtDecoder.js'));
 var util = require('util');
 var http = require('https');
