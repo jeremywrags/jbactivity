e0c716e9f04182e27d126faa6798bbbd5d387e7f
jeremywrags@gmail.com <jragsdale@jragsdale-ltm4.internal.salesforce.com>
2019-09-25 13:59:09 -0500

    Added steps to activity

--- routes/setup.js
+++ routes/setup.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/install', function(req, res, next) {
+  console.log("Loading install page");
   res.send('respond with a resource');
 });
 
